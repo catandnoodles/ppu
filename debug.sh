567721dc4a1d7f4d01527b5b10c336c02d246c8f
catandnoodles <nina.g.simon@gmail.com>
2022-07-26 20:29:51 -0700

    debug.sh done -- shaved off unneeded libs

--- debug.sh
+++ debug.sh
@@ -1,1 +1,1 @@
-tcc -std=c11 -Idir ./header_only_libs/ -gdwarf -shared -o ./out -bench
+tcc -std=c11 -lm -lSDL2 -lGL -lSDL2_image -Wall -Wno-unused -o ./bin/debug_$(date +"%Y_%m_%d_%I_%M_%p").out -Idir ./header_only_libs/ -gdwarf -shared -bench
